bb3e1946c72d6e81b0f16ef7ae264a143614877c
Jingrong Tian <work_id_tjr@163.com>
2019-09-15 14:54:21 +0800

    add question

---
+++ make_add_question/README.md
@@ -0,0 +1,23 @@
+# make add question
+
+## task
+
+
+### make question list
+
+* add questions vue at `./components`
+    * add method `addQuestions()`, use axios
+    * click submit call `addQuestions()`
+    * add sucess the jump to list view, and display message
+* add router at `./router.js`
+* add alert component
+* use alert component in list page
+
+### add add button on menu
+
+```
+<router-link class="btn btn-success" to="/question">Add<router-link>
+```
+
+## answer
+check standard answer by checking `./answer_project`
---
+++ make_add_question/answer_project/src/components/AddQuestion.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="add-question container">
+        <h1 class="page-header">Add Question</h1>
+        <form v-on:submit="addQuestion">
+            <div class="form-group">
+                <label for="question">Question</label>
+                <input type="text" class="form-control" v-model="question.question">
+            </div>
+            <div class="form-group">
+                <label for="answer">Answer</label>
+                <input type="text" class="form-control" v-model="question.answer">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AddQuestion',
+    data() {
+        return {
+            question: {}
+        }
+    },
+    methods: {
+        addQuestion(e) {
+            e.preventDefault();
+            let that = this;
+            this.axios
+                .post('http://localhost:3000/questions', this.question)
+                .then(function (response) {
+                    that.$router.push({path: '/', query: {message: 'Add question success!'}});
+                });
+        }
+    }
+}
+</script>
